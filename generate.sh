b89153ae77ce8b3423107bf4fd1daa517eb961b0
OganM <ogan.mancarci@gmail.com>
2022-11-09 08:17:48 -0800

    generation takes logs

--- generate.sh
+++ generate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # This file can be used to pass in a variable path to run all generate scripts
 # Rscript generate/fbm.R
-Rscript generate/compile.R
-Rscript generate/bootstrap.R
-Rscript generate/package_data.R
+Rscript generate/compile.R >generate/compile_log 2>generate/compile_log_err
+Rscript generate/bootstrap.R >generate/bootstrap_log 2>generate/bootstrap_log_err
+Rscript generate/package_data.R >generate/package_data_log 2>generate/package_data_log_err
