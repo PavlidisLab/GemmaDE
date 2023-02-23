ece37836d3e03f21b0a7a303d86ae7f8bcb52249
OganM <ogan.mancarci@gmail.com>
2023-02-23 05:40:47 -0800

    simplified ontology to resolve duplicates

--- generate/package_data.R
+++ generate/package_data.R
@@ -76,6 +76,7 @@ ONTOLOGIES$OntologyScope <- ONTOLOGIES$OntologyScope %>% as.factor()
 ONTOLOGIES.DEFS$Node_Long <- ONTOLOGIES.DEFS$Node_Long %>% as.factor()
 ONTOLOGIES.DEFS$OntologyScope <- ONTOLOGIES.DEFS$OntologyScope %>% as.factor()
 
+
 if (file.exists(.DATA_PATH)) {
   DATA.HOLDER <- readRDS(.DATA_PATH)
 } else {
@@ -87,14 +88,64 @@ DATA.HOLDER$artificial <- NULL
 ONTOLOGIES.DEFS <- fixOntoGenes()
 
 
+
+# simplified ontology defs removes the ontology scope, resolves ambiguous terms
+
+# identify duplicates and disambiguate based on majority
+dups <- ONTOLOGIES.DEFS %>% 
+  dplyr::select(Node_Long,Definition) %>% 
+  unique %$% Node_Long %>% {.[duplicated(.)]}
+
+# a bunch of heuristics to disambiguate
+dups_resolved <- dups %>% parallel::mclapply(function(x){
+  defs = ONTOLOGIES.DEFS %>% 
+    dplyr::filter(Node_Long == x) %$% 
+    Definition %>% as.character() %>% 
+    tolower() %>% 
+    table %>% 
+    sort(decreasing = TRUE)
+  if(length(defs)== 1 || defs[1] == defs[2]){
+    # if a majority is not present try to extract the context and listen to the original source
+    context <- x %>% stringr::str_extract("(?<=[a-z]/)[A-Z]*?(?=_)")
+    if(!is.na(context) && context == 'DOID'){
+      context = 'DO'
+    }
+    disambig <- ONTOLOGIES.DEFS %>% dplyr::filter(Node_Long == x & OntologyScope == context) %$% Definition 
+    if(length(disambig) == 1){
+      return(as.character(disambig))
+    } else{
+      # finally, if all fails, go with the longest non-number definition 
+      possible =  ONTOLOGIES.DEFS %>% dplyr::filter(Node_Long == x) %$% Definition
+      char_count <- possible %>% stringr::str_replace_all("[0-9]",'') %>% nchar()
+      return(as.character(possible[which.max(char_count)]))
+    }
+  } else{
+    # if a majority is present, go with it
+    return(names(defs[1]))
+  }
+},mc.cores = 16) %>% unlist()
+names(dups_resolved) = dups
+
+
+SIMPLIFIED.ONTOLOGY.DEFS <- ONTOLOGIES.DEFS %>% dplyr::select(Node_Long,Definition) %>% unique()
+
+SIMPLIFIED.ONTOLOGY.DEFS %<>% dplyr::filter(!Node_Long %in% names(dups_resolved))
+SIMPLIFIED.ONTOLOGY.DEFS %<>% rbind(
+  data.table(Node_Long = dups_resolved %>% names,
+             Definition = dups_resolved)
+)
+
+
+
 usethis::use_data(ONTOLOGIES,overwrite = TRUE)
 usethis::use_data(ONTOLOGIES.DEFS,overwrite = TRUE)
+usethis::use_data(SIMPLIFIED.ONTOLOGY.DEFS,overwrite = TRUE)
 
 
 # CACHE.BACKGROUND ------------
 CACHE.BACKGROUND <- lapply(names(DATA.HOLDER), function(x){
   ONTOLOGIES = ONTOLOGIES
-  ONTOLOGIES.DEFS = ONTOLOGIES.DEFS
+  # ONTOLOGIES.DEFS = ONTOLOGIES.DEFS
   precomputeTags(x)
 }) %>%
   setNames(names(DATA.HOLDER))
