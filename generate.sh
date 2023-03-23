2b117bce1f58214b070ea8a450c75e1f2a3bbc61
OganM <ogan.mancarci@gmail.com>
2023-03-22 18:33:37 -0700

    add filter generation to generate.sh

--- generate.sh
+++ generate.sh
@@ -3,4 +3,5 @@
 # Rscript generate/fbm.R
 Rscript generate/compile.R >generate/compile_log 2>generate/compile_log_err
 Rscript generate/bootstrap.R >generate/bootstrap_log 2>generate/bootstrap_log_err
+Rscript generate/filters.R > generate/filters_log 2> generate/filters_log_err
 Rscript generate/package_data.R >generate/package_data_log 2>generate/package_data_log_err
