749eb1f0ea2e15143082947c87a874bf4337a0e2
OganM <ogan.mancarci@gmail.com>
2023-05-09 18:24:42 -0700

    3.2.2 to report

--- report/whole_thesis.R
+++ report/whole_thesis.R
@@ -140,20 +140,36 @@ rbindlist(list(DATA.HOLDER$human@experiment.meta[, .(N = length(unique(ee.ID)), 
 
 # chapter 3.2 demonstration of gene list results -----------
 sex_results <- de_search(genes = c('RPS4Y1', 'EIF1AY', 'DDX3Y', 'KDM5D', 'XIST'),
-                         taxa = c('human'),geeq = FALSE)
+                         taxa = c('human'),geeq = TRUE)
+
+sex_results_nogeeq <- de_search(genes = c('RPS4Y1', 'EIF1AY', 'DDX3Y', 'KDM5D', 'XIST'),
+                                taxa = c('human'),geeq = FALSE)
 
 cell_type_results <- de_search(genes = c("Cyp4f15", "Grin2c", "Col4a5", "Heph", "Celsr1", "Egfr", "Lgi4", 
                                          "Slc38a3", "Aass", "Fkbp10", "Slc7a10", "Cyp2d22", "Cd38", "Cyp4f14", 
                                          "Cbs", "Slc1a2", "Emp2", "Axl", "Slc14a1", "Btbd17"),
                                taxa = 'mouse',geeq = TRUE)
 
+cell_type_results_nogeeq <- de_search(genes = c("Cyp4f15", "Grin2c", "Col4a5", "Heph", "Celsr1", "Egfr", "Lgi4", 
+                                                "Slc38a3", "Aass", "Fkbp10", "Slc7a10", "Cyp2d22", "Cd38", "Cyp4f14", 
+                                                "Cbs", "Slc1a2", "Emp2", "Axl", "Slc14a1", "Btbd17"),
+                                      taxa = 'mouse',geeq = FALSE)
+
 glucocorticoid_results <- de_search(genes = c("BCL6", "BIRC3", "CEBPD", "ERRFI1", "FBXL16", "FKBP5", "GADD45B", 
                                               "IRS2", "KLF9", "PDK4", "PER1", "RGCC", "RGS2", "SEC14L2", "SLC16A12", 
                                               "TFCP2L1", "TSC22D3"),
                                     taxa = 'human',geeq = TRUE)
 
+glucocorticoid_results_nogeeq <- de_search(genes = c("BCL6", "BIRC3", "CEBPD", "ERRFI1", "FBXL16", "FKBP5", "GADD45B", 
+                                                     "IRS2", "KLF9", "PDK4", "PER1", "RGCC", "RGS2", "SEC14L2", "SLC16A12", 
+                                                     "TFCP2L1", "TSC22D3"),
+                                           taxa = 'human',geeq = FALSE)
 
-heart_results <- de_search(genes = c('MYL7', 'NPPA', 'NPPB'),taxa = 'human')
+
+heart_results <- de_search(genes = c('MYL7', 'NPPA', 'NPPB'),taxa = 'human',geeq=TRUE)
+heart_results_nogeeq <- de_search(genes = c('MYL7', 'NPPA', 'NPPB'),taxa = 'human',geeq= FALSE)
+
+
 
 scz_genes = c("HARS", "HARS2", "HCN1", "HIRIP3", "HSPA9", "HSPD1", "HSPE1", 
               "IGSF9B", "IK", "IMMP2L", "INA", "INO80E", "IREB2", "ITIH1", 
@@ -203,47 +219,55 @@ scz_genes = c("HARS", "HARS2", "HCN1", "HIRIP3", "HSPA9", "HSPD1", "HSPE1",
 scz_results <- de_search(genes = scz_genes,
                          taxa = 'human', geeq = TRUE)
 
+scz_results_nogeeq <- de_search(genes = scz_genes,
+                                taxa = 'human', geeq = FALSE)
 
-depression_results <- de_search(genes = c("SORCS3", "RBFOX1", "GRM5", "HIST1H2BN", "SHISA9", "TCF4", 
-                                          "NEGR1", "HIST1H3J", "DENND1A", "DCC", "RSRC1", "TENM2", "TMEM161B", 
-                                          "DRD2", "PGBD1", "ZKSCAN4", "HIST1H1B", "ERBB4", "ZKSCAN8", "BTN3A2", 
-                                          "PCLO", "ZSCAN16", "ZSCAN9", "TMEM106B", "MEF2C", "OLFM4", "GRM8", 
-                                          "ZNF165", "LRFN5", "OR2B2", "HIST1H2BL", "ZCCHC7", "B3GALTL", 
-                                          "BTN2A1", "ZSCAN26", "RERE", "CDH13", "ASTN2", "CACNA1E", "HIST1H2AL", 
-                                          "HLA-B", "HIST1H4L", "ZSCAN12", "CHD6", "CTNNA3", "METTL9", "MEGF11", 
-                                          "ZSCAN31", "ZNF197", "KLC1", "ZNF660", "SPPL3", "YLPM1", "PCDH9", 
-                                          "ZNF445", "ZKSCAN7", "HIST1H3I", "LIN28B", "PAX5", "PROX2", "FAM172A", 
-                                          "CELF4", "DLST", "NRG1", "SGCZ", "OR12D3", "RAB27B", "IGSF6", 
-                                          "GPC6", "PAX6", "SGIP1", "KDM3A", "C16orf45", "DCDC5", "ESR2", 
-                                          "LRP1B", "EMILIN3", "TRMT61A", "XRCC3", "SOX5", "CNTNAP5", "SEMA6D", 
-                                          "ANKK1", "ZFHX4", "LST1", "PRSS16", "TYR", "RFWD2", "PQLC2L", 
-                                          "BTN1A1", "DCDC1", "ZDHHC21", "TTC12", "SDK1", "APOPT1", "VRK2", 
-                                          "CABP1", "ZKSCAN3", "SAMD5", "ADCK3", "DENND1B", "TAOK3", "HS6ST3", 
-                                          "MYRF", "RTN1", "PSORS1C1", "CKB", "SF3B1", "FADS2", "GTF2IRD1", 
-                                          "NRD1", "ZC3H7B", "AREL1", "RANGAP1", "ZNF184", "ZDHHC5", "HIST1H2BF", 
-                                          "FAM120A", "KIF15", "NKAPL", "FCF1", "SORBS3", "PCDHA2", "PCDHA1", 
-                                          "PRR34", "SCYL1", "MR1", "BTN3A3", "TCTEX1D1", "CELF2", "CTNND1", 
-                                          "HSPA1A", "ASCC3", "ELAVL2", "HIST1H2BO", "RPS6KL1", "PCDHA3", 
-                                          "TRMT10C", "ABT1", "SCAI", "FADS1", "CTTNBP2", "KMT2A", "BEND4", 
-                                          "ESRRG", "BAZ2B", "GPC5", "IQCJ-SCHIP1", "TCAIM", "TMX2", "SLC17A3", 
-                                          "MED19", "ZNF638", "CDH22", "GRIK5", "HARS", "HSPE1-MOB4", "EP300", 
-                                          "HLA-DQB1", "PCNP", "ZHX3", "BCHE", "CRB1", "C3orf84", "MICB", 
-                                          "SLC30A9", "MARK3", "FHIT", "MARCH10", "CDK14", "PLCG1", "PSORS1C2", 
-                                          "AP3B1", "POGZ", "TRAF3", "CSMD1", "TMEM67", "PCDHA4", "TOPAZ1", 
-                                          "PMFBP1", "CNTN5", "INPP4B", "ZNF322", "ASIC2", "PLA2R1", "CHMP3", 
-                                          "SOX6", "PCDHA5", "FANCL", "ZNF35", "TMEM42", "KIAA1143", "C11orf31", 
-                                          "ACVR1B", "ZNF501", "RFTN2", "TMEM258", "TAL1", "NICN1", "HLA-DQA1", 
-                                          "ACTL8", "MOB4", "CCDC36", "PCDHA6", "STK19", "RHOA", "MAP9", 
-                                          "FNIP2", "RBMS3", "PLCL1", "SLC44A4", "LTBP3", "SPRY2", "C7orf72", 
-                                          "HTT", "UBE2M", "OTX2", "BAG5", "CDH9", "LPIN3", "EPHB2", "HMGN4", 
-                                          "PPP6C", "NOX4", "PRR16", "EXT1", "MGAT4C", "EYS", "STAU1", "HARS2", 
-                                          "BAD", "MYBPC3", "ETFDH", "SIM1", "FH", "ANKS1B", "ITPR3", "RABEPK", 
-                                          "RHOBTB1", "BSN", "RAB3B", "ZNF536", "TOP1", "CAMKK2", "MANEA", 
-                                          "ARHGEF25", "VPS41", "ATP1A3", "ITGB6", "ASXL3", "ANKHD1", "PCDHA7", 
-                                          "PTPRS", "CCS", "PHF2", "IK", "KYNU", "PPID", "FAM120AOS", "ZMAT2", 
-                                          "SERPING1", "USP3", "CACNA2D1", "ANKHD1-EIF4EBP3", "GINM1", "C1QTNF7", 
-                                          "MIER1", "SLC4A9", "PSEN2"),
-                                taxa = 'human')
+depression_genes = c("SORCS3", "RBFOX1", "GRM5", "HIST1H2BN", "SHISA9", "TCF4", 
+                     "NEGR1", "HIST1H3J", "DENND1A", "DCC", "RSRC1", "TENM2", "TMEM161B", 
+                     "DRD2", "PGBD1", "ZKSCAN4", "HIST1H1B", "ERBB4", "ZKSCAN8", "BTN3A2", 
+                     "PCLO", "ZSCAN16", "ZSCAN9", "TMEM106B", "MEF2C", "OLFM4", "GRM8", 
+                     "ZNF165", "LRFN5", "OR2B2", "HIST1H2BL", "ZCCHC7", "B3GALTL", 
+                     "BTN2A1", "ZSCAN26", "RERE", "CDH13", "ASTN2", "CACNA1E", "HIST1H2AL", 
+                     "HLA-B", "HIST1H4L", "ZSCAN12", "CHD6", "CTNNA3", "METTL9", "MEGF11", 
+                     "ZSCAN31", "ZNF197", "KLC1", "ZNF660", "SPPL3", "YLPM1", "PCDH9", 
+                     "ZNF445", "ZKSCAN7", "HIST1H3I", "LIN28B", "PAX5", "PROX2", "FAM172A", 
+                     "CELF4", "DLST", "NRG1", "SGCZ", "OR12D3", "RAB27B", "IGSF6", 
+                     "GPC6", "PAX6", "SGIP1", "KDM3A", "C16orf45", "DCDC5", "ESR2", 
+                     "LRP1B", "EMILIN3", "TRMT61A", "XRCC3", "SOX5", "CNTNAP5", "SEMA6D", 
+                     "ANKK1", "ZFHX4", "LST1", "PRSS16", "TYR", "RFWD2", "PQLC2L", 
+                     "BTN1A1", "DCDC1", "ZDHHC21", "TTC12", "SDK1", "APOPT1", "VRK2", 
+                     "CABP1", "ZKSCAN3", "SAMD5", "ADCK3", "DENND1B", "TAOK3", "HS6ST3", 
+                     "MYRF", "RTN1", "PSORS1C1", "CKB", "SF3B1", "FADS2", "GTF2IRD1", 
+                     "NRD1", "ZC3H7B", "AREL1", "RANGAP1", "ZNF184", "ZDHHC5", "HIST1H2BF", 
+                     "FAM120A", "KIF15", "NKAPL", "FCF1", "SORBS3", "PCDHA2", "PCDHA1", 
+                     "PRR34", "SCYL1", "MR1", "BTN3A3", "TCTEX1D1", "CELF2", "CTNND1", 
+                     "HSPA1A", "ASCC3", "ELAVL2", "HIST1H2BO", "RPS6KL1", "PCDHA3", 
+                     "TRMT10C", "ABT1", "SCAI", "FADS1", "CTTNBP2", "KMT2A", "BEND4", 
+                     "ESRRG", "BAZ2B", "GPC5", "IQCJ-SCHIP1", "TCAIM", "TMX2", "SLC17A3", 
+                     "MED19", "ZNF638", "CDH22", "GRIK5", "HARS", "HSPE1-MOB4", "EP300", 
+                     "HLA-DQB1", "PCNP", "ZHX3", "BCHE", "CRB1", "C3orf84", "MICB", 
+                     "SLC30A9", "MARK3", "FHIT", "MARCH10", "CDK14", "PLCG1", "PSORS1C2", 
+                     "AP3B1", "POGZ", "TRAF3", "CSMD1", "TMEM67", "PCDHA4", "TOPAZ1", 
+                     "PMFBP1", "CNTN5", "INPP4B", "ZNF322", "ASIC2", "PLA2R1", "CHMP3", 
+                     "SOX6", "PCDHA5", "FANCL", "ZNF35", "TMEM42", "KIAA1143", "C11orf31", 
+                     "ACVR1B", "ZNF501", "RFTN2", "TMEM258", "TAL1", "NICN1", "HLA-DQA1", 
+                     "ACTL8", "MOB4", "CCDC36", "PCDHA6", "STK19", "RHOA", "MAP9", 
+                     "FNIP2", "RBMS3", "PLCL1", "SLC44A4", "LTBP3", "SPRY2", "C7orf72", 
+                     "HTT", "UBE2M", "OTX2", "BAG5", "CDH9", "LPIN3", "EPHB2", "HMGN4", 
+                     "PPP6C", "NOX4", "PRR16", "EXT1", "MGAT4C", "EYS", "STAU1", "HARS2", 
+                     "BAD", "MYBPC3", "ETFDH", "SIM1", "FH", "ANKS1B", "ITPR3", "RABEPK", 
+                     "RHOBTB1", "BSN", "RAB3B", "ZNF536", "TOP1", "CAMKK2", "MANEA", 
+                     "ARHGEF25", "VPS41", "ATP1A3", "ITGB6", "ASXL3", "ANKHD1", "PCDHA7", 
+                     "PTPRS", "CCS", "PHF2", "IK", "KYNU", "PPID", "FAM120AOS", "ZMAT2", 
+                     "SERPING1", "USP3", "CACNA2D1", "ANKHD1-EIF4EBP3", "GINM1", "C1QTNF7", 
+                     "MIER1", "SLC4A9", "PSEN2")
+
+depression_results <- de_search(genes = depression_genes,
+                                taxa = 'human',geeq = TRUE)
+depression_results %<>% arrange(desc(`Test Statistic`))
+depression_results_nogeeq <- de_search(genes = depression_genes,
+                                       taxa = 'human',geeq = FALSE)
+depression_results_nogeeq %<>% arrange(desc(`Test Statistic`))
 
 # 3.2.1 Sex specific genes -----
 
@@ -252,21 +276,28 @@ sex_results %>% dplyr::arrange(desc(`Test Statistic`)) %>% {.[1,]}
 
 # RPS4Y1 alone has the strongest link on its own
 RPS4Y1 <- de_search(genes = c('RPS4Y1'),
-                         taxa = c('human'),geeq = TRUE)
+                    taxa = c('human'),geeq = TRUE)
 
 EIF1AY <- de_search(genes = c('EIF1AY'),
                     taxa = c('human'),geeq = TRUE)
 
 # 3.2.2 Cell type specific genes ------
+# top 20 astrocyte markers ranked by their main silhouette coefficient
+# I restricted the search to mouse studies
 
+astro_genes <- readr::read_delim(delim = ' ',
+                                 col_names = c('Gene','fc','mainsil','minsil'),
+                                 'https://raw.githubusercontent.com/PavlidisLab/neuroExpressoAnalysis/master/analysis/01.SelectGenes/Markers_1.2/Quick/All_CellTypes/Astrocyte')  %>%
+  arrange(desc(mainsil)) %$% Gene %>% {.[1:20]}
 
-
+astro_results <- de_search(astro_genes,taxa = 'mouse',geeq = TRUE)
+astro_results_no_geeq <- de_search(astro_genes,taxa = 'mouse',geeq = FALSE)
 
 # 3.2.5.1 Gemma DE can offer complementary insights to GO enrichment analysis -----
 
 # a small addition to see the effect of geeq on the results heree
 scz_results_no_geeq <- de_search(genes = scz_genes,
-                         taxa = 'human', geeq = FALSE)
+                                 taxa = 'human', geeq = FALSE)
 
 library(ermineR)
 
