71a63978a7c9ab45a35c059cc60a3ab8c3292478
OganM <ogan.mancarci@gmail.com>
2023-06-12 18:28:08 -0700

    force overwrite fbm

--- generate/pre_compute_scores.R
+++ generate/pre_compute_scores.R
@@ -4,6 +4,12 @@ data.holder = readRDS(file.path(RAWDIR, 'DATA.HOLDER.rds'))
 
 
 for (tax in names(data.holder)){
+  file.remove( file.path(DATADIR, 'fbm', tax, 'scores.bk'))
+  file.remove( file.path(DATADIR, 'fbm', tax, 'score.dimnames.rds'))
+  file.remove( file.path(DATADIR, 'fbm', tax, 'scores.rds'))
+  
+  
+  
   sc = data.holder[[tax]]@data$zscore*(1 - log10(Rfast::Pmax(matrix(1e-10, ncol = ncol(data.holder[[tax]]@data$adj.pv), nrow = nrow(data.holder[[tax]]@data$adj.pv)), data.holder[[tax]]@data$adj.pv)))
   
   dimnames(sc) %>% 
