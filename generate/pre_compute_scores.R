1a09135fde20213b0c8caabe871eab0af4b336d0
OganM <ogan.mancarci@gmail.com>
2023-06-12 18:20:13 -0700

    score pre-calculation

---
+++ generate/pre_compute_scores.R
@@ -0,0 +1,18 @@
+devtools::load_all()
+
+data.holder = readRDS(file.path(RAWDIR, 'DATA.HOLDER.rds'))
+
+
+for (tax in names(data.holder)){
+  sc = data.holder[[tax]]@data$zscore*(1 - log10(Rfast::Pmax(matrix(1e-10, ncol = ncol(data.holder[[tax]]@data$adj.pv), nrow = nrow(data.holder[[tax]]@data$adj.pv)), data.holder[[tax]]@data$adj.pv)))
+  
+  dimnames(sc) %>% 
+    rev() %>%
+    saveRDS(file.path(DATADIR, 'fbm', tax, 'score.dimnames.rds'))
+  
+  sc_fb = bigstatsr::as_FBM(sc %>% t,
+                    backingfile = file.path(DATADIR, 'fbm', tax, 'scores'),
+                    is_read_only = T)$save()
+}
+
+
