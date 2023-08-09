710fac89bfa34243e3d408601c04cfa4d702d06e
OganM <ogan.mancarci@gmail.com>
2023-08-09 16:32:13 -0700

    better loading of fbms

--- main/load.R
+++ main/load.R
@@ -1,5 +1,5 @@
 .DATA_PATH <- paste(DATADIR, 'DATA.HOLDER.light.rds', sep='/')
-
+fbm_path = file.path(DATADIR,'data_fbm')
 
 # Load the lite versions if they're already created.
 if (!exists("DATA.HOLDER")) {
@@ -7,44 +7,15 @@ if (!exists("DATA.HOLDER")) {
     DATA.HOLDER <- readRDS(.DATA_PATH)
     DATA.HOLDER$artificial <- NULL
   } else {
-    stop("Couldn't find DATA.HOLDER, run generate/fbm.R first.")
+    stop("Couldn't find DATA.HOLDER, run create_fbm.R first.")
   }
 }
 
 # Read existing FBMs
-for (i in names(DATA.HOLDER)) {
-  DATA.HOLDER[[i]]@data$zscore <- bigstatsr::big_attach(paste0(paste(DATADIR, 'fbm/', sep='/'), i, "/zscores.rds"))
-  attr(DATA.HOLDER[[i]]@data$zscore, ".dimnames") <- readRDS(paste0(paste(DATADIR, 'fbm/', sep='/'), i, "/z.dimnames.rds"))
-
-  DATA.HOLDER[[i]]@data$adj.pv <- bigstatsr::big_attach(paste0(paste(DATADIR, 'fbm/', sep='/'), i, "/adjpvs.rds"))
-  attr(DATA.HOLDER[[i]]@data$adj.pv, ".dimnames") <- readRDS(paste0(paste(DATADIR, 'fbm/', sep='/'), i, "/p.dimnames.rds"))
-  
-  score_file = paste0(file.path(DATADIR, 'fbm/'), i, "/scores.rds")
-  if(file.exists(score_file)){
-    DATA.HOLDER[[i]]@data$scores <- bigstatsr::big_attach(score_file)
-    attr(DATA.HOLDER[[i]]@data$scores, ".dimnames") <- readRDS(paste0(file.path(DATADIR, 'fbm/'), i, "/score.dimnames.rds"))
-  }
-  
-  score_contrast_file = paste0(file.path(DATADIR, 'fbm/'), i, "/scores_contrast.rds")
-  if(file.exists(score_contrast_file)){
-    DATA.HOLDER[[i]]@data$scores_contrast <- bigstatsr::big_attach(score_contrast_file)
-    attr(DATA.HOLDER[[i]]@data$scores_contrast, ".dimnames") <- readRDS(paste0(file.path(DATADIR, 'fbm/'), i, "/score_contrast.dimnames.rds"))
-  }
-  
-  
-  
-  fc_file = paste0(file.path(DATADIR, 'fbm/'), i, "/fc.rds")
-  if(file.exists(score_file)){
-    DATA.HOLDER[[i]]@data$fc <- bigstatsr::big_attach(fc_file)
-    attr(DATA.HOLDER[[i]]@data$fc, ".dimnames") <- readRDS(paste0(file.path(DATADIR, 'fbm/'), i, "/fc.dimnames.rds"))
-  }
-  
+for (t in names(DATA.HOLDER)) {
+  DATA.HOLDER[[t]]@data <- load_fbms(file.path(fbm_path,t),suffix = '_contrast')
 }
 
 gc()
-
-dimnames.FBM <- function(object, ...) {
-  attr(object, ".dimnames")
-}
 
 
